515690ce82f83689f6a07290cdee04631c169fe1
Felquis Gimenes <frgformenton@gmail.com>
2021-09-06 18:20:34 -0300

    Add entityDelete

--- frontend/src/data/EntityCreate.tsx
+++ frontend/src/data/EntityCreate.tsx
@@ -1,65 +1,78 @@
-import React, { useCallback } from 'react';
-import { useForm } from 'react-hook-form';
-import { useMutation } from '../../gqless';
-import ErrorMessage from '../ui/ErrorMessage';
+import React, { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import { useMutation } from "../../gqless";
+import ErrorMessage from "../ui/ErrorMessage";
 
-import { refetch } from './EntityList'
+import { refetch } from "./EntityList";
 
-const EntityCreate = ({  } : {  }) => {
-    const { handleSubmit, register } = useForm()
+const EntityCreate = ({}: {}) => {
+  const { handleSubmit, register } = useForm();
 
-    const [entityCreate, { isLoading, error }] = useMutation((mutation, {
+  const [entityCreate, { isLoading, error }] = useMutation(
+    (
+      mutation,
+      {
         type,
         value,
         entityId,
-    }: {
+      }: {
         type: string;
         value: string;
         entityId?: string;
-    }) => {
-        const data = mutation.entityCreate({type, value, entityId})
+      }
+    ) => {
+      const data = mutation.entityCreate({ type, value, entityId });
 
-        return { ...data }
-    })
+      return { ...data };
+    }
+  );
 
-    const onSubmit = useCallback((data) => {
-        entityCreate({
-            args: {
-                type: data.type,
-                value: data.value,
-                entityId: data.entityId,
-            }
-        }).then(() => {
-            refetch()
-        })
-    }, [entityCreate])
+  const onSubmit = useCallback(
+    (data) => {
+      entityCreate({
+        args: {
+          type: data.type,
+          value: data.value,
+          entityId: data.entityId,
+        },
+      }).then(() => {
+        refetch();
+      });
+    },
+    [entityCreate]
+  );
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <h2>EntityCreate</h2>
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h2>EntityCreate</h2>
 
-            <fieldset>
-                <label>Type</label>
-                <input {...register('type', { required: true })} disabled={isLoading} />
-            </fieldset>
+      <fieldset>
+        <label>Type</label>
+        <input {...register("type", { required: true })} disabled={isLoading} />
+      </fieldset>
 
-            <fieldset>
-                <label>Value</label>
-                <input {...register('value')} disabled={isLoading} />
-            </fieldset>
+      <fieldset>
+        <label>Value</label>
+        <input {...register("value")} disabled={isLoading} />
+      </fieldset>
 
-            <fieldset>
-                <label>Connect to Entity ID</label>
-                <input {...register('entityId')} disabled={isLoading} />
-            </fieldset>
+      <fieldset>
+        <label>Connect to Entity ID</label>
+        <input {...register("entityId")} disabled={isLoading} />
+      </fieldset>
 
-            <ErrorMessage error={error} />
+      <ErrorMessage error={error} />
 
-            <footer>
-                <button type="reset" disabled={isLoading}>Reset</button> <button type="submit" disabled={isLoading}>{isLoading ? 'Adding' : 'Add'}</button>
-            </footer>
-        </form>
-    );
+      <footer>
+        <button type="reset" disabled={isLoading}>
+          Reset
+        </button>{" "}
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? "Adding" : "Add"}
+        </button>
+      </footer>
+    </form>
+  );
 };
 
 export default EntityCreate;
--- frontend/src/ui/EntityRow.tsx
+++ frontend/src/ui/EntityRow.tsx
@@ -1,12 +1,56 @@
-import React from 'react';
-import { Entity } from '../../gqless';
+import React, { useCallback, useRef, useState } from "react";
+import { Entity, useMutation } from "../../gqless";
+import { refetch } from "../data/EntityList";
 
 const EntityRow = ({ entity }: { entity?: Entity }) => {
-    return (
-        <>
-            {entity?.type}{entity?.value ? `: ${entity?.value}` : ''} - {'"'}{entity?.id}{'"'}
-        </>
-    );
+  const selectRef = useRef<HTMLSelectElement>(null);
+  const [entityDelete] = useMutation((mutation, args: { id: string;}) => {
+    const data = mutation.entityDelete({
+      id: args.id,
+    });
+
+    return {...data}
+  });
+
+  const onDelete = useCallback(() => {
+    entityDelete({
+      args: {
+        id: entity?.id || ''
+      }
+    }).then(() => {
+      refetch()
+    })
+  }, [entityDelete, entity]);
+
+  const onChange = useCallback(
+    (event) => {
+      const action = event.target.value;
+
+      if (selectRef.current?.value) {
+        selectRef.current.value = "";
+        // setOpen(action);
+      }
+
+      if (action === 'delete') {
+        onDelete()
+      }
+    },
+    [selectRef, onDelete]
+  );
+
+  return (
+    <>
+      {entity?.type}
+      {entity?.value ? `: ${entity?.value}` : ""} - {'"'}
+      {entity?.id}
+      {'"'}
+
+      <select onChange={onChange} ref={selectRef}>
+        <option value="">option</option>
+        <option value="delete">delete</option>
+      </select>
+    </>
+  );
 };
 
 export default EntityRow;
