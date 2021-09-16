1927be181cb606e5dcde97a42e3d60926f8f21fc
Felquis G <frgformenton@gmail.com>
2021-09-16 01:31:17 -0300

    conntext: export redis pubsub

--- src/context.ts
+++ src/context.ts
@@ -1,13 +1,35 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+import { RedisPubSub } from "graphql-redis-subscriptions";
+import Redis, { RedisOptions } from "ioredis";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export interface Context {
-    prisma: PrismaClient
+  prisma: PrismaClient;
+  pubsub: RedisPubSub;
+}
+
+export let pubsub: RedisPubSub;
+
+if (process.env.NODE_ENV === "production") {
+  const options: RedisOptions = {
+    host: process.env.REDIS_HOST,
+    retryStrategy: (times: number) => {
+      return Math.min(times * 50, 2000);
+    },
+  };
+
+  pubsub = new RedisPubSub({
+    publisher: new Redis(options),
+    subscriber: new Redis(options),
+  });
+} else {
+  pubsub = new RedisPubSub();
 }
 
 export default function createContext() {
-    return {
-        prisma
-    }
+  return {
+    prisma,
+    pubsub,
+  };
 }
