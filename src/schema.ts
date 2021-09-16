2d7d25054b6bbe0dbc50546e891756d3ffd95d16
Felquis G <frgformenton@gmail.com>
2021-09-16 01:34:17 -0300

    apply prettier

--- src/schema.ts
+++ src/schema.ts
@@ -1,24 +1,22 @@
-import { connectionPlugin, makeSchema  } from 'nexus'
-import { nexusPrisma } from 'nexus-plugin-prisma'
+import { connectionPlugin, makeSchema } from "nexus";
+import { nexusPrisma } from "nexus-plugin-prisma";
 
-import * as path from 'path'
-import * as types from './graphql'
+import * as path from "path";
+import * as types from "./graphql";
 
 export const schema = makeSchema({
-    types: { ...types },
-    plugins: [
-      nexusPrisma(),
-      connectionPlugin({ includeNodesField: true })
-    ],
-    sourceTypes: {
-      modules: [{ module: '.prisma/client', alias: 'PrismaClient' }],
-    },
-    contextType: {
-      module: path.join(__dirname, 'context.ts'),
-      export: 'Context',
-    },
-    outputs: {
-      typegen: path.join(__dirname, '..', 'nexus-typegen.ts'),
-      schema: path.join(__dirname, '..', 'schema.graphql'),
-    },
-})
+  types: { ...types },
+  plugins: [nexusPrisma(), connectionPlugin({ includeNodesField: true })],
+  // shouldExitAfterGenerateArtifacts: true,
+  sourceTypes: {
+    modules: [{ module: ".prisma/client", alias: "PrismaClient" }],
+  },
+  contextType: {
+    module: path.join(__dirname, "context.ts"),
+    export: "Context",
+  },
+  outputs: {
+    typegen: path.join(__dirname, "..", "nexus-typegen.ts"),
+    schema: path.join(__dirname, "..", "schema.graphql"),
+  },
+});
